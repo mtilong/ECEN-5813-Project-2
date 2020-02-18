2864e9f9380fa1ebd52d0c2e2760ae7f8d59354f
mtilong <44181039+mtilong@users.noreply.github.com>
2020-02-17 18:36:17 -0700

    Add files via upload

---
+++ Debug/board/clock_config.d
@@ -0,0 +1,40 @@
+board/clock_config.o board/clock_config.d: ../board/clock_config.c \
+ /Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/drivers/fsl_smc.h \
+ /Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/drivers/fsl_common.h \
+ /Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/fsl_device_registers.h \
+ /Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/MKL25Z4.h \
+ /Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/core_cm0plus.h \
+ /Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/core_cmInstr.h \
+ /Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/cmsis_gcc.h \
+ /Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/core_cmFunc.h \
+ /Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/system_MKL25Z4.h \
+ /Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/MKL25Z4_features.h \
+ /Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/drivers/fsl_clock.h \
+ ../board/clock_config.h \
+ /Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/drivers/fsl_common.h
+
+/Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/drivers/fsl_smc.h:
+
+/Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/drivers/fsl_common.h:
+
+/Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/fsl_device_registers.h:
+
+/Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/MKL25Z4.h:
+
+/Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/core_cm0plus.h:
+
+/Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/core_cmInstr.h:
+
+/Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/cmsis_gcc.h:
+
+/Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/core_cmFunc.h:
+
+/Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/system_MKL25Z4.h:
+
+/Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/CMSIS/MKL25Z4_features.h:
+
+/Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/drivers/fsl_clock.h:
+
+../board/clock_config.h:
+
+/Users/mtilong/Desktop/MCUXpressoIDE_11.1.0/workspace/project2FB/drivers/fsl_common.h:
